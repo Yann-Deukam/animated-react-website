83e55e2de13e4df92c65611ad0989c4a94d99f8c
Yann-Donald <yanndonald237@gmail.com>
2025-05-30 02:14:21 +0100

    Portfolio section

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Hero from "./components/Hero";
 import About from "./components/About";
 import Navbar from "./components/Navbar";
+import Portfolio from "./components/Portfolio";
 
 const App = () => {
   return (
@@ -9,6 +10,7 @@ const App = () => {
       <Navbar />
       <Hero />
       <About />
+      <Portfolio />
     </main>
   );
 };
